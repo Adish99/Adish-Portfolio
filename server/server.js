ae133a4afd6256bf5ebb550f927252b2102aa274
Adish99 <karkiadish999@gmail.com>
2026-09-24 14:21:55 +0545

    Final debuging before deploying.

--- server/server.js
+++ server/server.js
@@ -9,7 +9,15 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+  })
+);
 app.use(express.json());
 
 // Test route
